57c59db79f18104086524385b1ae67a33fddebcc
Aivis Abele <aabele@gmail.com>
2017-08-15 12:08:09 +0300

    Fixed env variable names.

--- app/settings_production.py
+++ app/settings_production.py
@@ -1,13 +1,13 @@
 from app.settings_remote import *  # noqa
 
-SECRET_KEY = get_env_setting('DEVELOPMENT_SECRET_KEY')
+SECRET_KEY = get_env_setting('PRODUCTION_SECRET_KEY')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'huntercamera_web_development',
-        'USER': 'huntercamera_web_development_user',
-        'PASSWORD': get_env_setting('DEVELOPMENT_PASSWORD'),
+        'NAME': 'kasmanzvana_production',
+        'USER': 'kasmanzvana_production_user',
+        'PASSWORD': get_env_setting('PRODUCTION_PASSWORD'),
         'HOST': 'localhost',
         'CONN_MAX_AGE': 600  # DB connection max age - 10 mins
     }

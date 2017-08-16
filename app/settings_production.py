caf164baa1235f96f39596d08b2814ed0e3d0636
Aivis Abele <aabele@gmail.com>
2017-08-16 23:50:34 +0300

    Added production settings.

--- app/settings_production.py
+++ app/settings_production.py
@@ -26,3 +26,21 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 
 PRODUCTION = True
+
+INSTALLED_APPS += ('raven.contrib.django.raven_compat',)
+RAVEN_CONFIG = {
+    'dsn': 'https://efe407373a044897a69eff5e66523b04:22a7c13b77fa433ea549541ebe388550@sentry.io/183774',
+}
+
+EMAIL_USE_SSL = True
+EMAIL_HOST = 'mail.sigmanet.lv'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'rekins@skolutiesibas.lv'
+EMAIL_HOST_PASSWORD = 'rekins2016!'
+
+EMAIL_SUBJECT_PREFIX = '[kasmanzvana.lv] '
+SERVER_EMAIL = 'rekins@skolutiesibas.lv'
+
+ADMINS = (
+    ('Aivis Abele', 'aabele@gmail.com'),
+)

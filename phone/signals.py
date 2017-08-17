e98c0dbf6f3899c5c1c2b8c92d2ec5bfd4467d00
Aivis Abele <aabele@gmail.com>
2017-08-17 20:13:10 +0300

    Fixed new comment email notification

--- phone/signals.py
+++ phone/signals.py
@@ -18,6 +18,7 @@ def notify_admin_about_comment(sender, instance, created, **kwargs):
     if created:
         mail_admins(
             'New comment posted in kasmanzvana.lv',
-            'https://kasmanzvana.lv/{0}/auth/user/{1}/change/\n\n{2}\n\n{3}'.format(
-                ADMIN_URL, instance.pk, instance.phone, instance.body)
+            ('https://kasmanzvana.lv/{0}/phone/comment/{1}/change/\n\n'
+             'Telefons: {2}\n\n'
+             'Komentārs: {3}').format(ADMIN_URL, instance.pk, instance.phone, instance.body)
         )

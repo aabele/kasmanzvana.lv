c642c639176f10da658dc05fa3d53037735c7e0a
Aivis Abele <aabele@gmail.com>
2017-08-24 14:50:42 +0300

    Search by phone.

--- phone/admin.py
+++ phone/admin.py
@@ -1,10 +1,14 @@
-
+"""
+Application model admin configuration.
+"""
 from django.contrib import admin
 from phone import models
 
-admin.site.register([
-    models.Phone,
-])
+
+class PhoneAdmin(admin.ModelAdmin):
+    search_fields = ('phone', )
+
+admin.site.register(models.Phone, PhoneAdmin)
 
 
 class CommentAdmin(admin.ModelAdmin):

ac50ffb3d564b361ed34c1d21bc15796599daafc
Aivis Abele <aabele@gmail.com>
2017-08-24 15:06:18 +0300

    Send email notification about new visitor message.

---
+++ website/signals.py
@@ -0,0 +1,23 @@
+"""
+Application model signals
+"""
+
+from django.core.mail import mail_admins
+from django.db.models.signals import post_save
+from django.dispatch.dispatcher import receiver
+from app.urls import ADMIN_URL
+
+from website import models
+
+
+@receiver(post_save, sender=models.Message)
+def notify_admin_about_comment(sender, instance, created, **kwargs):
+    """
+    Send email notification about new message
+    """
+    if created:
+        mail_admins(
+            'New message from kasmanzvana.lv visitor',
+            ('https://kasmanzvana.lv/{0}/phone/comment/{1}/change/\n\n'
+             'Comment: {3}').format(ADMIN_URL, instance.pk, instance.body)
+        )
